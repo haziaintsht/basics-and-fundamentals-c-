5de27d4ec6c3be20ff2d390af8858a8f78b48329
jhay <jhay-vy_adajar@dlsl.edu.ph>
2025-02-03 09:06:20 +0800

    wehh di nga

--- titeeee.cpp
+++ titeeee.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main() {
 
-    cout << "Hello World";
+    cout << "Hello World and hi there!" << endl;
+    cout << "What the fuck is happening right now?";
+    cout << "wtf?";
     return 0;
 }

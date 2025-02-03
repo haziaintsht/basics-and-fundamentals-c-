c98b4571c0ae1bc080327475b766d5bb03473d20
jhay <jhay-vy_adajar@dlsl.edu.ph>
2025-02-03 09:23:29 +0800

    well well well

--- titeeee.cpp
+++ titeeee.cpp
@@ -6,5 +6,6 @@ int main() {
     cout << "Hello World and hi there!" << endl;
     cout << "What the fuck is happening right now?";
     cout << "wtf?";
+    cout << "heyyyy bro wyd?";
     return 0;
 }
